d30a698b8e39f53aba8b037ac626b2261644dc33
Seth <76290048+sethvance26@users.noreply.github.com>
2021-04-05 21:12:37 -0400

    Updating JS

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 console.log('hello');
 
+
+let map = $('map');
+
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 37.541, lng: -77.434 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
